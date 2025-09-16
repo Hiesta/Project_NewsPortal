8b3628efbf1632939e14cb4ddbb7977b93458280
Antonio <antoshka2003@bk.ru>
2025-09-16 23:46:05 +0300

    Project_NewsPortal: small-fix

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('main_page.urls')),
@@ -24,5 +25,5 @@ urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     path('accounts/', include('allauth.urls')),
     path('articles/', include('main_page.urls_articles')),
-    path('auth/', include('authorization.urls'))
+    path('auth/', include('authorization.urls')),
 ]
